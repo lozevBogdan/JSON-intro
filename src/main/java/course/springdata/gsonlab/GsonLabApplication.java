68e1bcab47e9bdad8723182542c886dcc1b5f4f8
lozev <lozev.bogdan@abv.bg>
2020-11-15 16:05:00 +0200

    Skeleton

---
+++ src/main/java/course/springdata/gsonlab/GsonLabApplication.java
@@ -0,0 +1,13 @@
+package course.springdata.gsonlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsonLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GsonLabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/course/springdata/gsonlab/GsonLabApplicationTests.java
@@ -0,0 +1,13 @@
+package course.springdata.gsonlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsonLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
